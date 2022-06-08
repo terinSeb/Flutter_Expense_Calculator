02196a70112d01fec8319ccbbb7a3e9c659aeae1
Terin Sebastin <terin01@gmail.com>
2022-06-08 03:35:07 +0100

    Added TextField and TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:intl/intl.dart';
+
 import './transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -32,8 +34,8 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //crossAxisAlignment: CrossAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children:  [
         // ignore: sized_box_for_whitespace
         Container(
@@ -42,6 +44,20 @@ class MyHomePage extends StatelessWidget {
             color: Colors.blue,
             child: Text('chart'),)
           ),
+          // ignore: prefer_const_literals_to_create_immutables
+          Card(child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [                      
+              const TextField(decoration: InputDecoration(labelText: 'Title'),),
+              const TextField(decoration: InputDecoration(labelText: 'Amount'),),
+              TextButton(
+                onPressed: () {}, 
+                child:  const Text('Add Transactions',style: TextStyle(color: Colors.purple)),
+                )
+            ],),
+          ),),
         Column(
           children:  transaction.map((tx) => 
            Card(
@@ -53,7 +69,7 @@ class MyHomePage extends StatelessWidget {
                  padding: const EdgeInsets.all(10),
                  child: 
                  Text(
-                   tx.amount.toString(),
+                   '\$${tx.amount}',                   
                    style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.purple),),
                  ),
                Column(
@@ -61,7 +77,7 @@ class MyHomePage extends StatelessWidget {
                  children: [                
                  Text(tx.title, 
                  style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 16,)),
-                 Text(tx.date.toString(),
+                 Text(DateFormat().add_yMMMd().format(tx.date),
                  style: const TextStyle(color: Colors.grey),)
                ],)
              ],),)
