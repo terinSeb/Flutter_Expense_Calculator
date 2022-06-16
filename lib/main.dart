0a548f7cf4be8e9d28ec893a8b616773e3b56814
Terin Sebastin <terin01@gmail.com>
2022-06-16 03:25:13 +0100

    Added functionality to delete a Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:expense_calculator/widgets/chart.dart';
 
 import './widgets/transaction_list.dart';
@@ -8,85 +7,93 @@ import 'package:flutter/material.dart';
 
 import 'models/transaction.dart';
 
-void main() => runApp( MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-   MyApp({Key? key}) : super(key: key);
-final ThemeData theme = ThemeData();
+  MyApp({Key? key}) : super(key: key);
+  final ThemeData theme = ThemeData();
   @override
   Widget build(BuildContext context) {
-    
     // ignore: prefer_const_constructors
-    return  MaterialApp(
+    return MaterialApp(
       title: 'Flutter App',
-      home:  MyHomePage(),
-      theme: 
-      ThemeData(
-        primarySwatch: Colors.red,       
-        // accentColor: Colors.amber,    
-        fontFamily: 'Quicksand',
-    
-        textTheme: ThemeData.light().textTheme.copyWith(
-            headline6:const TextStyle(fontFamily: 'Opensans',fontSize: 20,fontWeight: FontWeight.bold,color: Colors.purple),
-            button: const TextStyle(color: Colors.white)
-          ),
+      home: MyHomePage(),
+      theme: ThemeData(
+          primarySwatch: Colors.red,
+          // accentColor: Colors.amber,
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                  fontFamily: 'Opensans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple),
+              button: const TextStyle(color: Colors.white)),
           appBarTheme: const AppBarTheme(
-                      titleTextStyle: TextStyle(
-                      fontFamily: 'OpenSans',
-                      fontSize: 31,
-                      fontWeight: FontWeight.bold
-                      ),
-                     
-)
-        // appBarTheme: AppBarTheme(
-        //   // ignore: deprecated_member_use
-        //   textTheme: ThemeData.light().textTheme.copyWith(
-        //     headline6:const TextStyle(fontFamily: 'Opensans',fontSize: 50,fontWeight: FontWeight.bold)
-        //   )
-        )
-    ,
-      
-    //    theme.copyWith(
-    // colorScheme: theme.colorScheme.copyWith(secondary: Colors.amber),
+            titleTextStyle: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 31,
+                fontWeight: FontWeight.bold),
+          )
+          // appBarTheme: AppBarTheme(
+          //   // ignore: deprecated_member_use
+          //   textTheme: ThemeData.light().textTheme.copyWith(
+          //     headline6:const TextStyle(fontFamily: 'Opensans',fontSize: 50,fontWeight: FontWeight.bold)
+          //   )
+          ),
+
+      //    theme.copyWith(
+      // colorScheme: theme.colorScheme.copyWith(secondary: Colors.amber),
     );
   }
 }
 
 // ignore: use_key_in_widget_constructors
 class MyHomePage extends StatefulWidget {
-  
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-final List<Transaction> _userTransaction = [
+  final List<Transaction> _userTransaction = [
     //  Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     //   Transaction(id: 't2', title: 'Weekly groceries', amount: 16.53, date: DateTime.now())
-   ];
-   List<Transaction> get _recentTransaction{
+  ];
+  List<Transaction> get _recentTransaction {
     return _userTransaction.where((tx) {
       return tx.date.isAfter(DateTime.now().subtract(const Duration(days: 7)));
     }).toList();
-   }
-   void addTransaction(String txTitle, double txAmount,DateTime chooseDate){
- final newTx = Transaction(id:  DateTime.now().toString(), title: txTitle, amount: txAmount, date:  chooseDate);
- setState(() {
-   _userTransaction.add(newTx);
- });
-}
+  }
 
+  void addTransaction(String txTitle, double txAmount, DateTime chooseDate) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: chooseDate);
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
 
- void _shoAddNewTransaction(BuildContext ctx){
-   showModalBottomSheet(context: ctx, builder: (_) {
-return GestureDetector(onTap: () {},
-behavior: HitTestBehavior.opaque,
-child: NewTransaction(addTx: addTransaction) ,
-);
- 
-   },);
- }
+  void _deleteTransaction(String id) {
+    setState(() {
+      _userTransaction.removeWhere((element) => element.id == id);
+    });
+  }
+
+  void _shoAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+          child: NewTransaction(addTx: addTransaction),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -94,28 +101,35 @@ child: NewTransaction(addTx: addTransaction) ,
       appBar: AppBar(
         title: const Text('Flutter App'),
         actions: [
-          IconButton(onPressed:  () => _shoAddNewTransaction(context), icon: const Icon(Icons.add))
+          IconButton(
+              onPressed: () => _shoAddNewTransaction(context),
+              icon: const Icon(Icons.add))
         ],
-      ),      
-      body: SingleChildScrollView(        
+      ),
+      body: SingleChildScrollView(
         child: Column(
           //mainAxisAlignment: MainAxisAlignment.spaceAround,
           // crossAxisAlignment: CrossAxisAlignment.center,
-          children:  [
-          // ignore: sized_box_for_whitespace
-          // Container(
-          //   width: double.infinity,
-          //   child: const Card(
-          //     color: Colors.blue,
-          //     child: Text('chart'),)
-          //   ),
-             Chart(transactions: _recentTransaction) ,
-        TransactionList(transactions: _userTransaction,)
-  
-          ],),
+          children: [
+            // ignore: sized_box_for_whitespace
+            // Container(
+            //   width: double.infinity,
+            //   child: const Card(
+            //     color: Colors.blue,
+            //     child: Text('chart'),)
+            //   ),
+            Chart(transactions: _recentTransaction),
+            TransactionList(
+              transactions: _userTransaction,
+              deleteTX: _deleteTransaction,
+            )
+          ],
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(onPressed: () => _shoAddNewTransaction(context), child: const Icon(Icons.add)),
-      );
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _shoAddNewTransaction(context),
+          child: const Icon(Icons.add)),
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,63 +3,70 @@ import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-
 class TransactionList extends StatelessWidget {
-  final  List<Transaction> transactions;
-   const TransactionList({Key? key,required this.transactions}) : super(key: key);
-
+  final List<Transaction> transactions;
+  final Function deleteTX;
+  const TransactionList(
+      {Key? key, required this.transactions, required this.deleteTX})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // ignore: sized_box_for_whitespace
-    return  Container(
+    return Container(
       height: 200,
-      child: transactions.isEmpty ? 
-       Column(children: [
-        Text("No transaction added yet!",
-        style: Theme.of(context).textTheme.headline6 ,),
-        const SizedBox(height: 10,),
-        
-        // ignore: sized_box_for_whitespace
-        Container(
-          height: 150,
-          child: Image.asset('assets/images/waiting.png',fit:BoxFit.cover))
-       ],) :
-       ListView.builder(
-        itemBuilder: (context, index) {
-        return   
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  "No transaction added yet!",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
 
-Card(
-  elevation: 5,
-  margin: const EdgeInsets.symmetric(vertical: 8,horizontal: 5),
-  child:   ListTile(
-  
-            leading: CircleAvatar(radius: 30,child:
-  
-            Padding(
-  
-              padding: const EdgeInsets.all(6),
-  
-              child: FittedBox(child: Text('\$${transactions[index].amount.toStringAsFixed(2)}', )),
-  
+                // ignore: sized_box_for_whitespace
+                Container(
+                    height: 150,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover))
+              ],
             )
-  
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6),
+                          child: FittedBox(
+                              child: Text(
+                            '\$${transactions[index].amount.toStringAsFixed(2)}',
+                          )),
+                        )),
+                    title: Text(transactions[index].title,
+                        style: Theme.of(context).textTheme.headline6),
+                    subtitle: Text(
+                      DateFormat().add_yMMMd().format(transactions[index].date),
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () {
+                        deleteTX(transactions[index].id);
+                      },
+                    ),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-  
-            title: Text(transactions[index].title, 
-  
-                       style:Theme.of(context).textTheme.headline6),
-  
-            subtitle: Text(DateFormat().add_yMMMd().format(transactions[index].date),
-  
-                       style: const TextStyle(color: Colors.grey),),
-  
-          ),
-);
-        },
-        itemCount: transactions.length  ,
-        
-      ),
     );
   }
 }
